d8cdaa96ea0eafe12a271942da30a61bb00aa81c
Jay Freeman <jaythinkshappiness@gmail.com>
2017-01-27 09:58:12 -0800

    modify classes in html, create variables and capture user clicked values/add to new Pizza

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,18 +3,32 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
 }
 
+var newOrderSize = []
+var newOrderToppings = []
+var size = newOrderSize
+var toppings = newOrderToppings
+var newPizza = new Pizza(size,toppings);
 
 
+console.log(toppings);
+console.log(newPizza);
+// var allToppings = ['vcheese','vpeperoni','vchicken','veggies','vmeatlovers']
 
-
+Pizza.prototype.shovetoppings = function() {
+  newPizza.push(newToppings)
+};
 
 
 
 $(document).ready(function() {
   $("#sizeOrder").submit(function(event) {
    event.preventDefault();
-   var pizzaSize = $("input:radio[name=flavor]:checked").val();
-   var orderToppings = $("input[type=checkbox]:checked").val();
-   console.log(orderToppings);
-  })
-})
+   var pizzaSize = $("input:radio[name=size]:checked").each(function(){
+    newOrderSize.push($(this).val());
+   var orderToppings = $("input:checkbox[name=topping]:checked").each(function(){
+     newOrderToppings.push($(this).val());
+   });
+   });
+  //  console.log(orderToppings);
+ });
+});
