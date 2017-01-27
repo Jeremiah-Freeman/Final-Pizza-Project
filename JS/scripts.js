04ded135c007f50659a3efcd5a2672282e76967c
Jeremiah <jaythinkshappiness@gmail.com>
2017-01-27 08:25:16 -0800

    updated HTML(form, submit button,radio and checkboxes), and JS code(doc ready, click-submit action)

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,3 +2,18 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+
+
+
+
+
+
+
+$(document).ready(function) {
+  $("#sizeOrder").submit(function(event) {
+   event.preventDefault();
+   var pizzaSize = $(".pizzaSize").val();
+   var orderToppings = $(".orderToppings").val();
+   console.log(orderToppings);
+}
