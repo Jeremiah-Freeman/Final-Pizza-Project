560c32f9c79bf72df87fed1551799dcdfc298639
Jay Freeman <bardo@bardo.local>
2017-02-01 23:51:39 -0800

    update js files with more attempts and solving functionality

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,43 +1,53 @@
-function Pizza(size, toppings) {
-  this.pizzaSize = size;
-  this.toppings = toppings;
-}
 
 var newOrderSize = [];
 var newOrderToppings = [];
-var pizzaSize = newOrderSize;
-var toppings = newOrderToppings;
-var orderedPizza = new Pizza(pizzaSize,toppings);
+newOrderToppings.toString();
+newOrderSize.toString();
+console.log(newOrderToppings);
 
-var total = newOrderToppings.split('').map(function(item) {
-  return parseInt(item, 10);
 
-Pizza.prototype.calculate = function() {
-for ( var i = 0; i < orderedPizza; i++) {
-    if (pizzaSize === 7){
-      return pizzaSize + 7;
-    } else if (pizzaSize === 9){
-      return pizzaSize + 9;
-    } else if (pizzaSize === 11){
-      return pizzaSize + 11;
-    }
-  }
-}//
+var thePizzaSize = newOrderSize;
+var someToppings = parseInt(newOrderToppings,10);
+
+function Pizza(size, toppings) {
+         this.pizzaSize = size;
+         this.toppings = toppings;
+       };
+
+var orderedPizza = new Pizza(newOrderSize,newOrderToppings);  //ordered pizza
+
+
+//
+// pizza.prototype.calculate = function() {
+//     for ( var i = 0; i <= orderedPizza; i++ )
+//     orderedPizza[i] + orderedPizza[i];
+//
+// };
+
+
+
+
+
 
 
 //front-end logic
 $(document).ready(function() {
   $("#sizeOrder").submit(function(event) {
-   event.preventDefault();
-   var pizzaSize = $("input:radio[name=size]:checked").each(function(){
-    newOrderSize.push($(this).val());
-   var orderToppings = $("input:checkbox[name=topping]:checked").each(function(){
-     newOrderToppings.push($(this).val());
-     var price = orderedPizza.calculate();
-     console.log(price);
-    // console.log(finalOrder);
-   });
-   });
-  //  console.log(orderToppings);
- });
+     event.preventDefault();
+
+
+     var pizzaSize = $("input:radio[name=size]:checked").each(function() {
+      newOrderSize.push($(this).val());
+     var orderToppings = $("input:checkbox[name=topping]:checked").each(function() {
+       newOrderToppings.push($(this).val());
+      //  alert(orderToppings);
+      //  var price = orderedPizza.calculate();
+      //  console.log(price);
+
+      });
+    });
+  });
 });
+
+
+//
